72a146b483268b49092e95312584358083050a91
Gerrit Vos <gerrit@radix-recruitment.be>
2014-01-12 10:57:24 +0000

    Drupal updates

--- sites/all/modules/gmap/js/icon.js
+++ sites/all/modules/gmap/js/icon.js
@@ -60,7 +60,6 @@ Drupal.gmap.getIcon = function (setname, sequence) {
              t.infoWindowAnchor = new GPoint(q.infoX, q.infoY);
              */
             p = Drupal.settings.basePath + q.path;
-/*             p = Drupal.settings.basePath + Drupal.settings.pathPrefix + q.path; */
             t = new google.maps.MarkerImage(p + q.sequence[i].f,
                 new google.maps.Size(q.sequence[i].w, q.sequence[i].h),
                 null,
